ccf619657985d4f11bfd5effd56f971b84c1c20e
aalexebr <alex.ebr@icloud.com>
2023-06-21 17:16:29 +0200

    ADD - select in html

--- js/script.js
+++ js/script.js
@@ -4,24 +4,53 @@ const submit = document.getElementById('submit');
 submit.addEventListener('submit',
     function(event){
         event.preventDefault();
-        
-        for(let i=1; i<=100; i++){
+        const select = document.getElementById('select').value;
+
+        if(select == 'hard'){
+            console.log('selected 10')
+            for(let i=1; i<=100; i++){
             
-            // document.querySelector(`main > .my-container`).innerHTML+=`<div class="box">
-            // ${i}
-            // </div>`
-            const box = document.createElement('div')
-            box.classList.add('box')  
-            box.innerHTML = i  
-            document.querySelector(' main > .my-container').append(box);
-            box.addEventListener('click',
-                function(){
-                    box.classList.toggle('bkg-color')
-                    console.log(i)
-                }
-            );
+                // document.querySelector(`main > .my-container`).innerHTML+=`<div class="box">
+                // ${i}
+                // </div>`
+                const box = document.createElement('div')
+                box.classList.add('box')  
+                box.innerHTML = i  
+                document.querySelector(' main > .my-container').append(box);
+                box.addEventListener('click',
+                    function(){
+                        box.classList.toggle('bkg-color')
+                        // console.log(i)
+                    }
+                );
+               
+            }
+    
+        };
+        if(select == 'medium'){
+            console.log('selected 9')
+        };
+        if(select == 'easy'){
+            console.log('selected 7')
+        };
+
+        // for(let i=1; i<=100; i++){
+            
+        //     // document.querySelector(`main > .my-container`).innerHTML+=`<div class="box">
+        //     // ${i}
+        //     // </div>`
+        //     const box = document.createElement('div')
+        //     box.classList.add('box')  
+        //     box.innerHTML = i  
+        //     document.querySelector(' main > .my-container').append(box);
+        //     box.addEventListener('click',
+        //         function(){
+        //             box.classList.toggle('bkg-color')
+        //             // console.log(i)
+        //         }
+        //     );
            
-        }
+        // }
 
     }
-)
+);
