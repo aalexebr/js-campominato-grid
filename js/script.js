523cfe272ad2fcfd848a67f4610e3d619c15ed70
aalexebr <alex.ebr@icloud.com>
2023-06-21 16:46:39 +0200

    ADD- click on box

--- js/script.js
+++ js/script.js
@@ -7,9 +7,20 @@ submit.addEventListener('submit',
         
         for(let i=1; i<=100; i++){
             
-            document.querySelector(`main > .my-container`).innerHTML+=`<div class="box">
-            ${i}
-            </div>`
+            // document.querySelector(`main > .my-container`).innerHTML+=`<div class="box">
+            // ${i}
+            // </div>`
+            const box = document.createElement('div')
+            box.classList.add('box')  
+            box.innerHTML = i  
+            document.querySelector(' main > .my-container').append(box);
+            box.addEventListener('click',
+                function(){
+                    box.classList.toggle('bkg-color')
+                    console.log(i)
+                }
+            );
+           
         }
 
     }
