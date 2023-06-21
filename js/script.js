a614b41147298996e544053c121fbf5199ab8ddb
aalexebr <alex.ebr@icloud.com>
2023-06-21 17:25:13 +0200

    ADD functions for select

--- js/script.js
+++ js/script.js
@@ -9,12 +9,28 @@ submit.addEventListener('submit',
         if(select == 'hard'){
             console.log('selected 10')
             for(let i=1; i<=100; i++){
-            
-                // document.querySelector(`main > .my-container`).innerHTML+=`<div class="box">
-                // ${i}
-                // </div>`
+
                 const box = document.createElement('div')
-                box.classList.add('box')  
+                box.classList.add('box','box-100')  
+                box.innerHTML = i  
+                document.querySelector(' main > .my-container').append(box);
+                box.addEventListener('click',
+                    function(){
+                        box.classList.toggle('bkg-color')
+                        // console.log(i)
+                    }
+                );
+               
+            };
+    
+        };
+
+        if(select == 'medium'){
+            console.log('selected 9')
+            for(let i=1; i<=81; i++){
+
+                const box = document.createElement('div')
+                box.classList.add('box','box-81')  
                 box.innerHTML = i  
                 document.querySelector(' main > .my-container').append(box);
                 box.addEventListener('click',
@@ -25,13 +41,24 @@ submit.addEventListener('submit',
                 );
                
             }
-    
         };
-        if(select == 'medium'){
-            console.log('selected 9')
-        };
+
         if(select == 'easy'){
             console.log('selected 7')
+            for(let i=1; i<=49; i++){
+
+                const box = document.createElement('div')
+                box.classList.add('box','box-49')  
+                box.innerHTML = i  
+                document.querySelector(' main > .my-container').append(box);
+                box.addEventListener('click',
+                    function(){
+                        box.classList.toggle('bkg-color')
+                        // console.log(i)
+                    }
+                );
+               
+            }
         };
 
         // for(let i=1; i<=100; i++){
